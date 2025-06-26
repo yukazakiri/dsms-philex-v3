1c40a4c3b2264f86fbc1fb28552b557ea25e2f1d
Louis <128908897+yukazakiri@users.noreply.github.com>
2025-06-26 19:12:03 +0800

    Add core application configuration and settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+final class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
@@ -19,6 +28,64 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureDates();
+        $this->configureModels();
+        $this->configureUrl();
+        $this->configureVite();
+    }
+
+    /**
+     * It's recommended to use CarbonImmutable as it's immutable and thread-safe to avoid issues with mutability.
+     *
+     * @see https://dyrynda.com.au/blog/laravel-immutable-dates
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(App::isProduction());
+    }
+
+    /**
+     * Configure the application's models.
+     * This is optional, but it's recommended to enable strict mode and disable mass assignment.
+     *
+     * @see https://laravel.com/docs/eloquent#configuring-eloquent-strictness
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+
+        Model::unguard();
+    }
+
+    /**
+     * Configure the application's URL.
+     * This is optional, but it's recommended to force HTTPS in production.
+     *
+     * @see https://laravel.com/docs/octane#serving-your-application-via-https
+     */
+    private function configureUrl(): void
+    {
+        URL::forceHttps(App::isProduction());
+        // $this->app['request']->server->set('HTTPS', true); ///here ;
+
+
+    }
+
+    /**
+     * Configure the application's Vite loading strategy.
+     * This is optional, but it's recommended to use aggressive prefetching so the UI feels snappy.
+     */
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
     }
 }
