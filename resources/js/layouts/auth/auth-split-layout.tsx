04fa5575dbb4633a0b43d8a0fe38c9780ba35f29
Louis <128908897+yukazakiri@users.noreply.github.com>
2025-06-26 20:18:56 +0800

    Add cover image to auth split layout

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -12,25 +12,21 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
     const { name, quote } = usePage<SharedData>().props;
 
     return (
-        <div className="h-screen flex items-center justify-center">
-            <div className="w-full h-full grid lg:grid-cols-2 p-4">
-                <div className="max-w-xs m-auto w-full flex flex-col items-center">
+        <div className="flex h-screen items-center justify-center">
+            <div className="grid h-full w-full p-4 lg:grid-cols-2">
+                <div className="m-auto flex w-full max-w-xs flex-col items-center">
                     <Link href={route('home')} className="flex items-center">
                         <AppLogoIcon className="h-9 w-9" />
                     </Link>
-                    
-                    {title && (
-                        <p className="mt-4 text-xl font-bold tracking-tight">{title}</p>
-                    )}
-                    {description && (
-                        <p className="text-muted-foreground text-sm text-balance">{description}</p>
-                    )}
-                    
-                    <div className="w-full mt-8">
-                        {children}
-                    </div>
+
+                    {title && <p className="mt-4 text-xl font-bold tracking-tight">{title}</p>}
+                    {description && <p className="text-sm text-balance text-muted-foreground">{description}</p>}
+
+                    <div className="mt-8 w-full">{children}</div>
                 </div>
-                <div className="bg-muted hidden lg:block rounded-lg" />
+                <div className="hidden rounded-lg bg-muted lg:block">
+                    <img src="/images/cover.jpg" className="h-full w-full rounded-lg object-cover brightness-90 filter" alt="" />
+                </div>
             </div>
         </div>
     );
